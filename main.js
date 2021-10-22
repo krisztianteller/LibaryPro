8c53fd752c8508fdb54680feeef882104a30ed4b
krisztianteller <75849856+krisztianteller@users.noreply.github.com>
2021-10-22 18:59:53 +0200

    popup window

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let libary = []
+/*let libary = []
 
 function book(){
 
@@ -6,4 +6,22 @@ function book(){
 
 function addBookToLibary(){
     
+}*/
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
+
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementsByTagName('span')[0];
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = form.title.value; 
+        this.author = form.author.value; 
+        this.pages = form.pages.value + 'pg'; 
+        this.read = form.read.checked; 
+    }
 }
