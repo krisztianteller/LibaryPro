f5cd7b98e9d3aeda472c40e479cf6fb703dd8db7
krisztianteller <75849856+krisztianteller@users.noreply.github.com>
2021-10-29 19:53:16 +0200

    addbook function

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
-/*let libary = []
 
-function book(){
-
-}
-
-function addBookToLibary(){
+function addBook() {
     
-}*/
-const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', addBookToLibrary);
-
-const newBookBtn = document.querySelector('#newBtn');
-newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
-
-const popUpForm = document.getElementById('popUp');
-const closePopUp = document.getElementsByTagName('span')[0];
-closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
-
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = form.title.value; 
-        this.author = form.author.value; 
-        this.pages = form.pages.value + 'pg'; 
-        this.read = form.read.checked; 
-    }
+    let newBook = document.createElement("DIV");
+    let title = document.createElement("INPUT");
+    let author = document.createElement("INPUT");
+    let pages = document.createElement("INPUT");
+    author.setAttribute("type", "text");
+    pages.setAttribute("type", "text");
+    title.setAttribute("type", "text");
+    title.value = "Title";
+    author.value = "Author"
+    pages.value = "Pages"
+    document.body.appendChild(newBook);
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(pages);
 }
